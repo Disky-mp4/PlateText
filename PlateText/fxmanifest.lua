ed2450a634530d93dd9b774a5591d6b3797e07c4
Disky <73770320+Disky-mp4@users.noreply.github.com>
2021-11-20 09:36:56 -0500

    altered fxmanifest
    Forgot to alter the description after finishing the project.

--- PlateText/fxmanifest.lua
+++ PlateText/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'Disky#9999' -- Discord
-description '/license [argument] - Sets the player\'s license plate to the argument.'
+description '/changeplate [desired plate text] - Sets the player\'s license plate to the (first) argument.'
 version '1.0.0'
 
 client_script 'client.js'
